6120eaebcd99ccd8410aea06c5228ffa0e013fcc
gibisetti <giovanna_bisetti@hotmail.com>
2019-05-23 02:23:55 -0300

    show and hide

--- duvidas/js/script.js
+++ duvidas/js/script.js
@@ -2,9 +2,11 @@ let mostrar = document.getElementById('show');
 let esconder = document.getElementById('hide');
 let pergunta = document.getElementById('pergunta');
 let resposta = document.getElementById('resposta');
+let botoes = document.querySelector('.btnShow');
 
 
-pergunta.addEventListener('click' , function() {
+// mostrar/esconder p clicando na pergunta
+pergunta.addEventListener('click', function () {
     if (resposta.style.display == "none") {
         resposta.style.display = "block";
         mostrar.classList.remove('active');
@@ -14,9 +16,10 @@ pergunta.addEventListener('click' , function() {
         mostrar.classList.add('active');
         esconder.classList.remove('active');
     }
-})
+});
 
-// mostrar/esconder p com botões
+
+// mostrar/esconder p clicando nos botões
 mostrar.addEventListener('click', function () {
     resposta.style.display = "block";
     mostrar.classList.remove('active');
@@ -27,13 +30,27 @@ esconder.addEventListener('click', function () {
     resposta.style.display = "none";
     mostrar.classList.add('active');
     esconder.classList.remove('active');
-
 });
+
+
+
+// // mostrar/esconder p botoes com 1 função
+// botoes.addEventListener('click' , function() {
+//     if (resposta.style.display == "none") {
+//         resposta.style.display = "block";
+//         mostrar.classList.remove('active');
+//         esconder.classList.add('active');
+//     } else {
+//         resposta.style.display = "none";
+//         mostrar.classList.add('active');
+//         esconder.classList.remove('active');
+//     }
+// });
 
 
 // // mostrar/esconder p clicando na pergunta com on click
 // function hideAndShow() {
-    
+
 //     if (resposta.style.display == "none") {
 //         resposta.style.display = "block";
 //         mostrar.classList.remove('active');
