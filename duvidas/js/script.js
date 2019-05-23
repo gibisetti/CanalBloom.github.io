cb248ea4b400b1814adedbdff93d9a0b102e982e
gibisetti <giovanna_bisetti@hotmail.com>
2019-05-23 18:15:21 -0300

    hide and show done

--- duvidas/js/script.js
+++ duvidas/js/script.js
@@ -1,63 +1,179 @@
-let mostrar = document.getElementById('show');
-let esconder = document.getElementById('hide');
-let pergunta = document.getElementById('pergunta');
-let resposta = document.getElementById('resposta');
-let botoes = document.querySelector('.btnShow');
-
-
-// mostrar/esconder p clicando na pergunta
-pergunta.addEventListener('click', function () {
-    if (resposta.style.display == "none") {
-        resposta.style.display = "block";
-        mostrar.classList.remove('active');
-        esconder.classList.add('active');
+//Question 01
+let q1 = document.getElementById('q1');
+let r1 = document.getElementById('r1');
+let show1 = document.getElementById('show1');
+let hide1 = document.getElementById('hide1');
+q1.addEventListener('click', function () {
+    if (r1.style.display == "none") {
+        r1.style.display = "block";
+        show1.classList.remove('active');
+        hide1.classList.add('active');
     } else {
-        resposta.style.display = "none";
-        mostrar.classList.add('active');
-        esconder.classList.remove('active');
+        r1.style.display = "none";
+        show1.classList.add('active');
+        hide1.classList.remove('active');
     }
 });
 
 
-// mostrar/esconder p clicando nos botões
-mostrar.addEventListener('click', function () {
-    resposta.style.display = "block";
-    mostrar.classList.remove('active');
-    esconder.classList.add('active');
+//Question 02
+let q2 = document.getElementById('q2');
+let r2 = document.getElementById('r2');
+let show2 = document.getElementById('show2');
+let hide2 = document.getElementById('hide2');
+q2.addEventListener('click', function () {
+    if (r2.style.display == "none") {
+        r2.style.display = "block";
+        show2.classList.remove('active');
+        hide2.classList.add('active');
+    } else {
+        r2.style.display = "none";
+        show2.classList.add('active');
+        hide2.classList.remove('active');
+    }
 });
 
-esconder.addEventListener('click', function () {
-    resposta.style.display = "none";
-    mostrar.classList.add('active');
-    esconder.classList.remove('active');
+
+//Question 03
+let q3 = document.getElementById('q3');
+let r3 = document.getElementById('r3');
+let show3 = document.getElementById('show3');
+let hide3 = document.getElementById('hide3');
+q3.addEventListener('click', function () {
+    if (r3.style.display == "none") {
+        r3.style.display = "block";
+        show3.classList.remove('active');
+        hide3.classList.add('active');
+    } else {
+        r3.style.display = "none";
+        show3.classList.add('active');
+        hide3.classList.remove('active');
+    }
 });
 
 
+//Question 04
+let q4 = document.getElementById('q4');
+let r4 = document.getElementById('r4');
+let show4 = document.getElementById('show4');
+let hide4 = document.getElementById('hide4');
+q4.addEventListener('click', function () {
+    if (r4.style.display == "none") {
+        r4.style.display = "block";
+        show4.classList.remove('active');
+        hide4.classList.add('active');
+    } else {
+        r4.style.display = "none";
+        show4.classList.add('active');
+        hide4.classList.remove('active');
+    }
+});
 
-// // mostrar/esconder p botoes com 1 função
-// botoes.addEventListener('click' , function() {
-//     if (resposta.style.display == "none") {
-//         resposta.style.display = "block";
-//         mostrar.classList.remove('active');
-//         esconder.classList.add('active');
-//     } else {
-//         resposta.style.display = "none";
-//         mostrar.classList.add('active');
-//         esconder.classList.remove('active');
-//     }
-// });
+
+//Question 05
+let q5 = document.getElementById('q5');
+let r5 = document.getElementById('r5');
+let show5 = document.getElementById('show5');
+let hide5 = document.getElementById('hide5');
+q5.addEventListener('click', function () {
+    if (r5.style.display == "none") {
+        r5.style.display = "block";
+        show5.classList.remove('active');
+        hide5.classList.add('active');
+    } else {
+        r5.style.display = "none";
+        show5.classList.add('active');
+        hide5.classList.remove('active');
+    }
+});
 
 
-// // mostrar/esconder p clicando na pergunta com on click
-// function hideAndShow() {
+//Question 06
+let q6 = document.getElementById('q6');
+let r6 = document.getElementById('r6');
+let show6 = document.getElementById('show6');
+let hide6 = document.getElementById('hide6');
+q6.addEventListener('click', function () {
+    if (r6.style.display == "none") {
+        r6.style.display = "block";
+        show6.classList.remove('active');
+        hide6.classList.add('active');
+    } else {
+        r6.style.display = "none";
+        show6.classList.add('active');
+        hide6.classList.remove('active');
+    }
+});
 
-//     if (resposta.style.display == "none") {
-//         resposta.style.display = "block";
-//         mostrar.classList.remove('active');
-//         esconder.classList.add('active');
-//     } else {
-//         resposta.style.display = "none";
-//         mostrar.classList.add('active');
-//         esconder.classList.remove('active');
-//     }
-// }
+
+//Question 07
+let q7 = document.getElementById('q7');
+let r7 = document.getElementById('r7');
+let show7 = document.getElementById('show7');
+let hide7 = document.getElementById('hide7');
+q7.addEventListener('click', function () {
+    if (r7.style.display == "none") {
+        r7.style.display = "block";
+        show7.classList.remove('active');
+        hide7.classList.add('active');
+    } else {
+        r7.style.display = "none";
+        show7.classList.add('active');
+        hide7.classList.remove('active');
+    }
+});
+
+
+//Question 08
+let q8 = document.getElementById('q8');
+let r8 = document.getElementById('r8');
+let show8 = document.getElementById('show8');
+let hide8 = document.getElementById('hide8');
+q8.addEventListener('click', function () {
+    if (r8.style.display == "none") {
+        r8.style.display = "block";
+        show8.classList.remove('active');
+        hide8.classList.add('active');
+    } else {
+        r8.style.display = "none";
+        show8.classList.add('active');
+        hide8.classList.remove('active');
+    }
+});
+
+
+//Question 09
+let q9 = document.getElementById('q9');
+let r9 = document.getElementById('r9');
+let show9 = document.getElementById('show9');
+let hide9 = document.getElementById('hide9');
+q9.addEventListener('click', function () {
+    if (r9.style.display == "none") {
+        r9.style.display = "block";
+        show9.classList.remove('active');
+        hide9.classList.add('active');
+    } else {
+        r9.style.display = "none";
+        show9.classList.add('active');
+        hide9.classList.remove('active');
+    }
+});
+
+
+//Question 10
+let q10 = document.getElementById('q10');
+let r10 = document.getElementById('r10');
+let show10 = document.getElementById('show10');
+let hide10 = document.getElementById('hide10');
+q10.addEventListener('click', function () {
+    if (r10.style.display == "none") {
+        r10.style.display = "block";
+        show10.classList.remove('active');
+        hide10.classList.add('active');
+    } else {
+        r10.style.display = "none";
+        show10.classList.add('active');
+        hide10.classList.remove('active');
+    }
+});
+
